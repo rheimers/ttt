ead23fb52f3ea1fecdac06d4ee0e9b7889d08c09
rheimers <regineheimers@gmail.com>
2020-08-11 15:55:17 +0200

    Lifting state up

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,21 +3,42 @@ import React from "react";
 import Square from "./Square";
 
 export default function Board() {
-  const status = "Next player: X";
+  //wir legen State mit neun leeren Arrays an
+  const [squares, setSquares] = React.useState([
+    //ziel dass wir genau diesen Zustand haben
+    "🐨",
+    null,
+    "🐨",
+    "🐨",
+    null,
+    "🐷",
+    "🐷",
+    null,
+    null,
+  ]);
+  const status = "Next player: 🐷";
 
+  const handleClick = (index) => {
+    //Copy squares (copy array with spread operator, old way with slice)
+    const squaresUpdate = [...squares];
+    //Modify value by index
+    // squaresUpdate[index] = "🐷";
+    // Set new state
+    setSquares(squaresUpdate);
+  };
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board">
-        <Square value="0" />
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-        <Square value="7" />
-        <Square value="8" />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -5,19 +5,14 @@ export default function Square(props) {
   // wir legen Zustand an, und eine Funktion (setValue)
   //um zu aktualisieren, damit sich die Komponente -->
   // const [state, setState] = React.useState(initialValue);
-  const [value, setValue] = React.useState(null);
+  //const [value, setValue] = React.useState(null);
   // equals
   // const state = React.useState(null);
   // const value = state [0];
   // const setValue = state[1];
   return (
-    <button
-      className="square"
-      onClick={() => {
-        setValue("X");
-      }}
-    >
-      {value}
+    <button className="square" onClick={props.onClick}>
+      {props.value}
     </button>
   );
 }
